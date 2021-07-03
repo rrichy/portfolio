330cf57bd425c7f7e9076a6499d73c242caedffb
rrichy <rasulrichy@gmail.com>
2021-07-03 15:23:41 +0800

    home
    created a homepage with animation.
    but at homepage animation, only works once

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+const Home = ({ show, page }) => {
+  return (
+    <div
+      id="home"
+      className={!show ? "" : page === "home" ? "show-home" : "hide-home"}
+    >
+      <div id="data-home">
+        <h1>
+          Hi, I am&nbsp;
+          <span>RASUL RICHY</span>
+        </h1>
+        <h2>I'm a self-taught web developer.</h2>
+        <h2>I love writing codes.</h2>
+        <h2>I love learning new things.</h2>
+      </div>
+      <svg className="home-idle">
+        <circle cx="50%" cy="50%" r="217.5" />
+      </svg>
+    </div>
+  );
+};
+
+export default Home;
