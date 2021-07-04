d4cf98d92d5b21c93986a98f9f3e68190349af01
rrichy <rasulrichy@gmail.com>
2021-07-04 17:04:26 +0800

    bug fix
    bug on animation for changing pages fixed

---
+++ src/components/Projects.jsx
@@ -0,0 +1,7 @@
+// import {HighlightProject, NormalProject} from "./Projects/Project";
+
+const Projects = () => {
+  return <div id="projects"></div>;
+};
+
+export default Projects;
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,20 +3,30 @@ import { faAngleDoubleUp } from "@fortawesome/free-solid-svg-icons";
 
 import "./Welcome.css";
 
-const Welcome = ({ hide }) => {
+const Welcome = ({ show, nav }) => {
   return (
     <>
       <svg
         id="greet"
         viewBox="0 0 1350 600"
         onClick={() => {
-          document
-            .querySelector("#greet")
-            .firstChild.setAttribute("class", "hide");
+          const welcome = document.querySelector("#greet");
 
-          document.querySelector("#greet").lastChild.remove();
-          setInterval(() => hide(false), 1000);
+          // console.log(welcome.firstElementChild);
+          welcome.firstChild.setAttribute("class", "hide-welcome");
+          welcome.lastChild.remove();
+
+          // setTimeout(() => {
+          //   // document.querySelector("#home").className = "show-home";
+          //   console.log("hello!");
+          // }, 1200);
+
+          setTimeout(() => {
+            show(false);
+            nav(true);
+          }, 1000);
         }}
+        // className={show ? "" : "hide"}
       >
         <text x="50%" y="50%" textAnchor="middle" className="greet">
           Welcome
