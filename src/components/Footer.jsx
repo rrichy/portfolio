7e7b75337326707f2cb85468ad47bf72591d0b21
rrichy <rasulrichy@gmail.com>
2021-07-11 14:17:25 +0800

    update
    // done
    removed "footer" put back icons into nav
    added logo in nav
    
    //todo
    media queries
    intro page
    project storieshighlight projects readme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,4 @@
 import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faFreeCodeCamp,
-  faCodepen,
-} from "@fortawesome/free-brands-svg-icons";
 
 import "./styles/css/Footer.css";
 
@@ -14,33 +8,7 @@ const Footer = ({ showIcon, onProjects }) => {
   useEffect(() => setTimeout(() => animating(false), 700));
   if (!showIcon) return <footer></footer>;
   else {
-    return (
-      <footer className={onProjects ? "footer-on-projects" : ""}>
-        <div id="account-wrapper" className={onInitial ? "" : "default-icons"}>
-          <a
-            href="https://github.com/rrichy"
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a
-            href="https://codepen.io/rrichy"
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            <FontAwesomeIcon icon={faFreeCodeCamp} />
-          </a>
-          <a
-            href="https://www.freecodecamp.org/rrichy"
-            target="_blank"
-            referrerPolicy="no-referrer"
-          >
-            <FontAwesomeIcon icon={faCodepen} />
-          </a>
-        </div>
-      </footer>
-    );
+    return <footer className={onProjects ? "footer-on-projects" : ""}></footer>;
   }
 };
 
